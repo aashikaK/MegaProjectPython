83bdd941f3bf112fbd1777600d797bc488f7da93
Aashika <khatiwadaaashika12@gmail.com>
2026-04-19 08:36:53 +0545

    wake word

--- main.py
+++ main.py
@@ -12,3 +12,12 @@ def speak(text):
 
 if __name__=="__main__":
     speak("Hello! How are you doing? How may I help you?")
+    while True:
+        # Listening for wake word 'juice'
+        # obtain audio from the microphone
+        r=sr.Recognizer()
+        with sr.Microphone()
+
+        # recognize sppech using sphinx
+        try:
+            print
