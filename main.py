d7ce6e26f08609c265f54e2b014e64274c50a9b9
Aashika <khatiwadaaashika12@gmail.com>
2026-04-19 09:47:43 +0545

    wake word

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import speech_recognition as sr
 import webbrowser
 import pyttsx3
+import subprocess
 # pip install pocketsphinx
  
 
@@ -8,19 +9,26 @@ recognizer=sr.Recognizer()
 engine= pyttsx3.init()
 
 def speak(text):
-    engine.say(text)
-    engine.runAndWait()
+    # engine.say(text)
+    # engine.runAndWait()
+    # engine.stop()
+    subprocess.run(
+        ["powershell", "-Command", f'Add-Type -AssemblyName System.Speech; '
+         f'$s = New-Object System.Speech.Synthesis.SpeechSynthesizer; '
+         f'$s.Speak("{text}")'],
+        check=True
+    )
 
 def processCommand(c):
     pass
 
 if __name__=="__main__":
     speak("Hello!How may I help you?")
+    
     while True:
         # Listening for wake word 'fan'
         # obtain audio from the microphone
         r=sr.Recognizer()
-        
         # recognize speech using sphinx
         try:
             with sr.Microphone() as source:
@@ -28,14 +36,14 @@ if __name__=="__main__":
                 audio=r.listen(source,timeout=2,phrase_time_limit=2)
             
             word=r.recognize_google(audio)
-            if(word.lower()=="fan"):
+            if "fan" in word.lower():
                 speak("Ya")
                 with sr.Microphone() as source:
                     print('Active......')
                     audio=r.listen(source)
                     command= r.recognize_google(audio)
 
-                    processCommand()
+                processCommand(command)
                 
         except Exception as e:
             print("Didn't catch that.Error-> ",e)
