145f0bde8a23655dca0e0ec4721da36d4dde71c3
Aashika <khatiwadaaashika12@gmail.com>
2026-04-19 19:47:48 +0545

    adding musics

--- musicLibrary.py
+++ musicLibrary.py
@@ -1,5 +1,9 @@
 music={
     "stealth" : "https://youtu.be/U47Tr9BB_wE?si=Hc1Ejh7CvhsEd0pJ",
     "alone" : "https://youtu.be/vmQ0R2Ysonw?si=dJ92Y6V17OuzI-LE",
-    "inception" : "https://youtu.be/NDhCZMh9SY0?si=gA9RdRPDAVV4Kib4"
+    "inception" : "https://youtu.be/NDhCZMh9SY0?si=gA9RdRPDAVV4Kib4",
+    "faasle":'https://youtu.be/EiiOYwqk3A0?si=iyuCvUHuZda3vHma',
+    'baby':'https://youtu.be/kffacxfA7G4?si=UH8wPnmCvwKi_70r',
+    'yummy':'https://youtu.be/8EJ3zbKTWQ8?si=PVuD8n6_PrOtJ5BV',
+    'paaro' :'https://youtu.be/DxsDekHDKXo?si=id_szlbbttLFf4Uj'
 }
