433bc45dc1dad0b6e78a08b609222ea653f0bc10
aashikaK <138367806+aashikaK@users.noreply.github.com>
2026-04-28 20:39:01 +0545

    Enhance voice assistant with TTS and AI integration
    Added text-to-speech functionality using gTTS and pygame. Integrated OpenAI API for AI responses and modified news fetching to retrieve US headlines.

--- main.py
+++ main.py
@@ -4,25 +4,60 @@ import pyttsx3
 import subprocess
 import musicLibrary
 import requests
-
+from gtts import gTTS
+from openai import OpenAI
+import pygame
+import os
 # pip install pocketsphinx
  
 
 r=sr.Recognizer()
 engine= pyttsx3.init()
-newsapi="Your api key from news api"
+newsapi="NEWS API KEY"
 
-def speak(text):
+
+client = OpenAI(api_key='your's openAI api key')
+
+def ask_ai(prompt):
+    response = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[
+             {"role": "system", "content": "You are a helpful voice assistant.You give short responses"},
+            {"role": "user", "content": prompt}
+        ]
+    )
+    return response.choices[0].message.content
+
+
+
+def speak_old(text):
     engine.say(text)
     engine.runAndWait()
-    # engine.stop()
-    # subprocess.run(
-    #     ["powershell", "-Command", f'Add-Type -AssemblyName System.Speech; '
-    #      f'$s = New-Object System.Speech.Synthesis.SpeechSynthesizer; '
-    #      f'$s.Speak("{text}")'],
-    #     check=True
-    # )
+    engine.stop()
+#     # subprocess.run(
+#     #     ["powershell", "-Command", f'Add-Type -AssemblyName System.Speech; '
+#     #      f'$s = New-Object System.Speech.Synthesis.SpeechSynthesizer; '
+#     #      f'$s.Speak("{text}")'],
+#     #     check=True
+#     # )
 
+
+
+def speak(text):
+    tts=gTTS(text)
+    tts.save('temp.mp3')
+
+    pygame.mixer.init()
+    pygame.mixer.music.load('temp.mp3')
+    pygame.mixer.music.play()
+
+# keep the program running until the music stops playing
+    while pygame.mixer.music.get_busy():
+        pygame.time.Clock().tick(10)
+    
+    pygame.mixer.music.unload()
+    os.remove('temp.mp3')
+    
 def processCommand(c):
     # print("Command:", c)
     # input("Press Enter to continue...")
@@ -44,15 +79,20 @@ def processCommand(c):
         webbrowser.open(link)
 
     elif "news" in c.lower():
-        r= requests.get(f"https://newsapi.org/v2/top-headlines?country=np&apiKey={newsapi}")
-        if r.status_code==200:
-            data = r.json()
+        r= requests.get(f"https://newsapi.org/v2/top-headlines?country=us&apiKey={newsapi}")
+        # if r.status_code==200:
+        data = r.json()
 
-            for article in data["articles"][:5]:   # top 5 headlines
-                print(article["title"])
-                speak(article["title"]) 
+        headlines = [article["title"] for article in data["articles"][:5]]
+
+        for i in range(0, len(headlines), 2):   # 2 at a time
+            chunk = ". ".join(headlines[i:i+2])
+            print(chunk)
+            speak(chunk)
     else:
         # let openAi handle the requests
+        output=ask_ai(c)
+        speak(output)
 
 
 if __name__=="__main__":
@@ -66,7 +106,7 @@ if __name__=="__main__":
         try:
             with sr.Microphone() as source:
                 print('Listening....')
-                audio=r.listen(source,timeout=2,phrase_time_limit=2)
+                audio=r.listen(source,timeout=5,phrase_time_limit=2)
             
             word=r.recognize_google(audio)
             if "moon" in word.lower():
